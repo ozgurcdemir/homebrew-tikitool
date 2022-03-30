7a9d2aff0f62ae8c6d6a4c9755a7691e6aeba8db
Ozgur Demir <ozgur.demir@itutor.com>
2022-03-29 17:21:45 -0700

    fixes

--- formula/tikitool.rb
+++ formula/tikitool.rb
@@ -1,9 +1,11 @@
 class Tikitool < Formula
     desc "Tikitool"
     homepage "https://github.com/bcpitutor/gobase/tikitool"
+    version "1.0.12"
+
+
     url "https://github.com/bcpitutor/gobase/releases/download/v1.0.12/tikitool_darwin_amd64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "4beeb6c33bd16b505ebe938bb8ffe211e2e76ef2867eccb72afee9907eae8366"
-    head "https://github.com/bcpitutor/gobase.git"
   
     def install
       bin.install "tikitool"

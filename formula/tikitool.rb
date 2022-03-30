e64800ca9871a86ab4d165c8d09fb30bd64f3ebc
Ozgur Demir <ozgur.demir@itutor.com>
2022-03-29 17:32:08 -0700

    fix

--- formula/tikitool.rb
+++ formula/tikitool.rb
@@ -3,11 +3,11 @@ require_relative "lib/private_strategy"
 
 class Tikitool < Formula
     desc "Tikitool"
-    homepage "https://github.com/bcpitutor/gobase/tikitool"
+    homepage "https://github.com/ozgurcdemir/homebrew-tikitool"
     version "1.0.12"
 
 
-    url "https://github.com/bcpitutor/gobase/releases/download/v1.0.12/tikitool_darwin_amd64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/ozgurcdemir/homebrew-tikitool/blob/main/binaries/tikitool_darwin_amd64"
     sha256 "4beeb6c33bd16b505ebe938bb8ffe211e2e76ef2867eccb72afee9907eae8366"
   
     def install
@@ -16,6 +16,6 @@ class Tikitool < Formula
   
     # Homebrew requires tests.
     test do
-      assert_match "\"v1.0.12\"", shell_output("#{bin}/mytool version | grep version| cut -d':' -f2", 2)
+      assert_match "\"v1.0.12\"", shell_output("#{bin}/tikitool version | grep version| cut -d':' -f2", 2)
     end
 end

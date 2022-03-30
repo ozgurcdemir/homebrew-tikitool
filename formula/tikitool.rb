3f767b1cef4d28d171a1198114e2752c31b2a606
Ozgur Demir <ozgur.demir@itutor.com>
2022-03-29 17:08:44 -0700

    initial

---
+++ formula/tikitool.rb
@@ -0,0 +1,16 @@
+class Tikitool < Formula
+    desc "Tikitool"
+    homepage "https://github.com/bcpitutor/gobase/tikitool"
+    url ".../tikitool-darwin-amd64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "4beeb6c33bd16b505ebe938bb8ffe211e2e76ef2867eccb72afee9907eae8366"
+    head "https://github.com/bcpitutor/gobase.git"
+  
+    def install
+      bin.install "tikitool"
+    end
+  
+    # Homebrew requires tests.
+    test do
+      assert_match "\"v1.0.12\"", shell_output("#{bin}/mytool version | grep version| cut -d':' -f2", 2)
+    end
+end

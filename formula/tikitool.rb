f01a2bd4e087489d10a3850e9b9b6237e8738d98
Ubuntu <Ozgur Demir>
2022-04-18 18:19:38 +0000

    tikitool.rb updated for v1.1.29, automatically with voodoo magic

--- formula/tikitool.rb
+++ formula/tikitool.rb
@@ -3,19 +3,19 @@ require "formula"
 class Tikitool < Formula
   desc "Tikitool"
   homepage "https://github.com/ozgurcdemir/homebrew-tikitool"
-  version "v1.1.28"
+  version "v1.1.29"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ozgurcdemir/homebrew-tikitool/releases/download/v1.1.28/tikitool_darwin_arm64"
-      sha256 "bf4b7cb23e1123eb2b688a0482a88f7efdb6f6a5013f15d961bfb9e38a66c3a3"
+      url "https://github.com/ozgurcdemir/homebrew-tikitool/releases/download/v1.1.29/tikitool_darwin_arm64"
+      sha256 "e971ddc3045987856cc496ab553865a3cd067d7b78ad6553bddf5e6f9e874a8a"
     
       def install
         bin.install "tikitool_darwin_arm64" => "tikitool"
       end
     end
      if Hardware::CPU.intel?
-       url "https://github.com/ozgurcdemir/homebrew-tikitool/releases/download/v1.1.28/tikitool_darwin_amd64"
-       sha256 "da94f895f488bdba308158f0dcd939591787aa7434f7ca4b4137b93f091e3a62"
+       url "https://github.com/ozgurcdemir/homebrew-tikitool/releases/download/v1.1.29/tikitool_darwin_amd64"
+       sha256 "c10959231adab1bfc739eb8471d6c04fa641546ad3d4094da8ca85e86131372a"
  
       def install
          bin.install "tikitool_darwin_amd64" => "tikitool"
@@ -25,8 +25,8 @@ class Tikitool < Formula
  
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ozgurcdemir/homebrew-tikitool/releases/download/v1.1.28/tikitool_linux_amd64"
-      sha256 "816f60b164a7e4646e4ceee6a035260d81edc0f43ecf3e2935f848512ac1dae1"
+      url "https://github.com/ozgurcdemir/homebrew-tikitool/releases/download/v1.1.29/tikitool_linux_amd64"
+      sha256 "64026b79c950b49319fd95bfd440f5ba59fef300d099864177654b168e907d1e"
  
       def install
         bin.install "tikitool_linux_amd64" => "tikitool"
